3e149cc3c0dbd89a8a0272637ec5aef3da1aaeae
kachilala <kachilala23@gmail.com>
2021-04-29 01:51:03 +0100

    refactored from call back to promise

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const superagent = require("superagent");
 const axios = require("axios");
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`The dog breed is ${data}`);
+    console.log(`The dog breed ${data}`);
 
     superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-        if(err) return console.log(err.message);
+    .then(res => {
         console.log(res.body.message);
-
+        
         fs.writeFile('dog-img.txt', res.body.message, err => {
+            if(err) return console.log(err.message);
             console.log('Random dog image saved to file');
         });
-    });
+    }).catch(err => {
+        console.log(err.message);
+    })
 });
 
 
